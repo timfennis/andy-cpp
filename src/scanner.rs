c4a48c706b9e252e2abd32c098ad08941c2da6e0
Tim Fennis <tim.fennis@nos.nl>
2023-12-16 10:56:52 +0100

    improved token scanning and added tests

--- src/scanner.rs
+++ src/scanner.rs
@@ -5,7 +5,7 @@ use std::fmt::{Display, Formatter};
 use std::iter::Peekable;
 use std::str::Chars;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 enum TokenType {
     // Single character
     LeftParentheses,
@@ -48,7 +48,7 @@ enum TokenType {
 
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct Token {
     kind: TokenType,
     line: usize,
@@ -77,49 +77,56 @@ impl Iterator for Scanner<'_> {
         'iterator: while let Some(char) = self.source.next() {
             self.column += 1;
             let peek = self.source.peek();
-            let token_type = match (char, peek) {
-                ('(', _) => TokenType::LeftParentheses,
-                (')', _) => TokenType::RightParentheses,
-                ('{', _) => TokenType::LeftBrace,
-                ('}', _) => TokenType::RightBrace,
-                (',', _) => TokenType::Comma,
-                ('-', _) => TokenType::Minus,
-                ('+', _) => TokenType::Plus,
-                (';', _) => TokenType::Semicolon,
-                ('*', _) => TokenType::Star,
-                ('!', Some('=')) => TokenType::BangEqual,
-                ('!', Some(_)) => TokenType::Bang,
-                ('=', Some('=')) => TokenType::EqualEqual,
-                ('=', Some(_)) => TokenType::Equal,
-                ('>', Some('=')) => TokenType::GreaterEqual,
-                ('>', Some(_)) => TokenType::Greater,
-                ('<', Some('=')) => TokenType::LessEqual,
-                ('<', Some(_)) => TokenType::Less,
+            let (token_type, consume_next) = match (char, peek) {
+                ('(', _) => (TokenType::LeftParentheses, false),
+                (')', _) => (TokenType::RightParentheses, false),
+                ('{', _) => (TokenType::LeftBrace, false),
+                ('}', _) => (TokenType::RightBrace, false),
+                (',', _) => (TokenType::Comma, false),
+                ('-', _) => (TokenType::Minus, false),
+                ('+', _) => (TokenType::Plus, false),
+                (';', _) => (TokenType::Semicolon, false),
+                ('*', _) => (TokenType::Star, false),
+                ('!', Some('=')) => (TokenType::BangEqual, true),
+                ('!', _) => (TokenType::Bang, false),
+                ('=', Some('=')) => (TokenType::EqualEqual, true),
+                ('=', _) => (TokenType::Equal, false),
+                ('>', Some('=')) => (TokenType::GreaterEqual, true),
+                ('>', _) => (TokenType::Greater, false),
+                ('<', Some('=')) => (TokenType::LessEqual, true),
+                ('<', _) => (TokenType::Less, false),
                 ('/', Some('/')) => {
                     // We ran into a doc comment and we just keep consuming characters as long as we
                     // don't encounter a linebreak
                     while let Some(ch) = self.source.peek() {
-                        if *ch != '\n' {
-                            self.source.next();
+                        if *ch == '\n' {
+                            continue 'iterator;
                         }
+
+                        self.source.next();
                     }
 
                     continue 'iterator;
                 }
-                ('/', Some(_)) => TokenType::Slash,
+                ('/', Some(_)) => (TokenType::Slash, false),
                 (' ' | '\t' | '\r', _) => {
                     continue 'iterator;
-                },
+                }
                 ('\n', _) => {
                     self.line += 1;
                     self.column = 0;
                     continue 'iterator;
-                },
+                }
                 (c, _) => {
                     // In case of an error we still push back the next character so we can continue parsing
-                    return Some(Err(ScannerError::UnexpectedCharacter(c)));
+                    return Some(Err(ScannerError::UnexpectedCharacter { char: c, line: self.line, column: self.column }));
                 }
             };
+
+            if consume_next {
+                // swallow the next char
+                self.source.next();
+            }
 
             return Some(Ok(Token {
                 kind: token_type,
@@ -132,15 +139,26 @@ impl Iterator for Scanner<'_> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum ScannerError {
-    UnexpectedCharacter(char),
+    UnexpectedCharacter { char: char, line: usize, column: usize },
 }
 
 impl Display for ScannerError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            ScannerError::UnexpectedCharacter(c) => write!(f, "Unexpected character '{}'", c),
+            ScannerError::UnexpectedCharacter { char, line, column } => write!(f, "Unexpected character '{char}' at line {line} column {column}"),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::scanner::{Scanner, Token};
+
+    #[test]
+    fn test_that_it_produces_no_errors() {
+        let scanner = Scanner::from_str(include_str!("../tests/scanner.andy"));
+        assert!(scanner.collect::<Result<Vec<Token>, _>>().is_ok());
     }
 }
