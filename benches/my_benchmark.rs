33ad28beccaf3cd6dfef31b94e40843b1556fe02
Tim Fennis <fennis.tim@gmail.com>
2024-12-31 09:36:04 +0100

    Add more benchmarks

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,27 +1,37 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::fs;
+use std::path::Path;
 
 use ndc_lib::interpreter::{Interpreter, InterpreterError};
 
 fn run_string(input: &str) -> Result<String, InterpreterError> {
     let buf: Vec<u8> = vec![];
     let mut interpreter = Interpreter::new(Box::new(buf));
+    // TODO: Is this black_box needed?
     interpreter.run_str(black_box(input), false)
 }
 
-fn addition(a: i32, b: i32) {
-    let result = run_string(&format!("{} + {}", a, b)).unwrap();
-    assert_eq!(result, format!("{}", a + b))
-}
-
-fn fibonacci() {
-    let program = "fn fib(n) { if n <= 1 { return 1 }; fib(n - 1) + fib(n - 2) }\nfib(20)";
-    let result = run_string(program).unwrap();
-    assert_eq!(result, "10946");
-}
-
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("addition", |b| b.iter(|| addition(5, 5)));
-    c.bench_function("fibonacci", |b| b.iter(fibonacci));
+    let dir = Path::new("benches/programs/");
+    let mut names = Vec::new();
+    match fs::read_dir(dir) {
+        Ok(files) => {
+            for file in files {
+                let file = file.unwrap();
+                let program = fs::read_to_string(file.path()).unwrap();
+                names.push(file.file_name().to_owned());
+                let name = names
+                    .last()
+                    .and_then(|n| n.to_str())
+                    .expect("must have a name");
+                let name = &name[0..name.len() - 4];
+                c.bench_function(name, |b| b.iter(|| run_string(&program)));
+            }
+        }
+        Err(_) => {
+            panic!("failed to read files from directory");
+        }
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
