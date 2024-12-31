8408955af656de1b22cd8d1aad2740707f5ff8b9
Tim Fennis <fennis.tim@gmail.com>
2024-12-31 13:29:59 +0100

    Improved math benchmarks

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -14,40 +14,57 @@ fn run_string(input: &str) -> Result<String, InterpreterError> {
 }
 
 fn math_benches(c: &mut Criterion) {
+    let mut group = c.benchmark_group("math");
     let mut rng = ChaCha8Rng::seed_from_u64(31011991);
-    let operators = ["+", "-", "*", "/", "\\", "^", "%", "%%"];
+    let operators = [
+        ("+", "addition"),
+        ("-", "subtraction"),
+        ("*", "multiplication"),
+        ("/", "division"),
+        ("\\", "floor division"),
+        ("^", "exponentiation"),
+        ("%", "modulo"),
+        ("%%", "remainder"),
+    ];
     let types = ["int", "float", "rational", "complex"];
 
+    let gen_num = || rng.gen_range(-10_000..=10_000);
+    let gen_exp = || rng.gen_range(1..=25);
+    let gen_rat = || rng.gen_range(1..=10_000);
     for typ in types {
-        for operator in operators {
+        for (operator, operator_name) in operators {
             let mut program = String::new();
             for _ in 0..100 {
-                let left = rng.gen_range(-10_000..=10_000);
+                let left = gen_num;
                 let right = match operator {
-                    "^" => rng.gen_range(1..=25),
-                    "/" => rng.gen_range(1..10_000),
-                    _ => rng.gen_range(-10_000..=10_000),
+                    "^" => gen_exp,
+                    "/" => gen_rat,
+                    _ => gen_num,
                 };
                 program.push_str(&match typ {
-                    "int" => format!("{left} {operator} {right};\n"),
-                    "float" => format!("{left}.0 {operator} {right}.0;\n"),
+                    "int" => format!("{} {operator} {};\n", left(), right()),
+                    "float" => format!("{}.0 {operator} {}.0;\n", left(), right()),
                     "rational" => format!(
-                        "({left}/{}) {operator} ({right}/{});\n",
-                        rng.gen_range(1..=10_000),
-                        rng.gen_range(1..=10_000),
+                        "({}/{}) {operator} ({}/{});\n",
+                        left(),
+                        left(),
+                        right(),
+                        right(),
                     ),
                     "complex" => {
                         format!(
-                            "({left}.0 + {}.0i) {operator} ({right}.0 + {}.0i);\n",
-                            rng.gen_range(-10_000..=10_000),
-                            rng.gen_range(-10_000..=10_000),
+                            "({}.0 + {}.0i) {operator} ({}.0 + {}.0i);\n",
+                            left(),
+                            left(),
+                            right(),
+                            right(),
                         )
                     }
                     _ => unreachable!(),
                 });
             }
 
-            c.bench_function(&format!("operator {} {}", operator, typ), |b| {
+            group.bench_function(&format!("{typ} {operator_name}"), |b| {
                 b.iter(|| run_string(&program))
             });
             println!("{program}");
