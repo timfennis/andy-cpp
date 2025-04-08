201f8512a1f84ed93fc8cd8a715b169ff4292630
Tim Fennis <fennis.tim@gmail.com>
2025-04-08 16:40:18 +0200

    Added missing documentation to many functions

--- ndc_lib/src/stdlib/hash_map.rs
+++ ndc_lib/src/stdlib/hash_map.rs
@@ -7,30 +7,48 @@ use std::rc::Rc;
 
 #[ndc_macros::export_module]
 mod inner {
-    // TODO: this function makes a copy when returning some kind of iterator would be better
+    /// Returns a list of all the keys in the map or set.
+    ///
+    /// Note that for a set this will return the values in the set.
     pub fn keys(map: &mut HashMap<Value, Value>) -> Value {
         map.keys().cloned().collect::<Vec<_>>().into()
     }
 
-    // TODO: this function makes a copy when returning some kind of iterator would be better
+    /// Returns a list of all the values in the map.
+    ///
+    /// Note that for sets this will return a list of unit types, you should use keys if you want the values in the set.
     pub fn values(map: &mut HashMap<Value, Value>) -> Value {
         map.values().cloned().collect::<Vec<_>>().into()
     }
 
+    /// Removes a key from the map or a value from a set.
     pub fn remove(map: &mut HashMap<Value, Value>, key: &Value) {
         map.remove(key);
     }
 
+    /// Removes all keys from the `left` map/set that are present in the `right` map/set.
+    #[function(name = "remove")]
+    pub fn remove_map(left: &mut HashMap<Value, Value>, right: &HashMap<Value, Value>) {
+        for (key, _) in right {
+            left.remove(key);
+        }
+    }
+
+    /// Insert a value into a map.
     #[function(name = "insert")]
     pub fn insert_map(map: &mut HashMap<Value, Value>, key: Value, value: Value) {
         map.insert(key, value);
     }
 
+    /// Inserts a value into a set.
     #[function(name = "insert")]
     pub fn insert_set(map: &mut HashMap<Value, Value>, key: Value) {
         map.insert(key, Value::unit());
     }
 
+    /// Returns the union (elements that are in either `left` or `right`) of two maps or sets.
+    ///
+    /// This is the same as evaluating the expression `left | right`
     pub fn union(left: DefaultMap<'_>, right: &HashMap<Value, Value>) -> Value {
         Value::Sequence(Sequence::Map(
             Rc::new(RefCell::new(hash_map::union(left.0, right))),
@@ -38,6 +56,9 @@ mod inner {
         ))
     }
 
+    /// Returns the intersection (elements that are in both `left and `right`) of two maps or sets.
+    ///
+    /// This is the same as evaluating the expression `left & right`.
     pub fn intersection(left: DefaultMap<'_>, right: &HashMap<Value, Value>) -> Value {
         Value::Sequence(Sequence::Map(
             Rc::new(RefCell::new(hash_map::intersection(left.0, right))),
@@ -45,6 +66,9 @@ mod inner {
         ))
     }
 
+    /// Returns the symmetric difference (elements that are either in `left` or `right` but not both) of two maps or sets.
+    ///
+    /// This is the same as evaluating the expression `left ~ right`.
     pub fn symmetric_difference(left: DefaultMap<'_>, right: &HashMap<Value, Value>) -> Value {
         Value::Sequence(Sequence::Map(
             Rc::new(RefCell::new(hash_map::symmetric_difference(left.0, right))),
@@ -52,6 +76,7 @@ mod inner {
         ))
     }
 
+    /// Converts the given sequence to set.
     pub fn set(seq: &mut Sequence) -> Value {
         let out: HashMap<Value, Value> = match seq {
             Sequence::String(rc) => rc
@@ -59,7 +84,6 @@ mod inner {
                 .chars()
                 .map(|c| (c.into(), Value::unit()))
                 .collect(),
-            // TODO: we could change the implementation so that ref counts of 1 are consumed instead of copied
             Sequence::List(rc) => rc
                 .borrow()
                 .iter()
